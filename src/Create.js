4459ae047c058317d8197ee115cd24eefd3818f5
DzmitryKryvou <qwertty1988@gmail.com>
2022-04-22 19:38:00 +0300

    Making a POST Request
    Making a POST Request + Submit Events +Programmatic Redirect

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,32 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 const Create = () => {
      const[title, setTitle] = useState('');
      const[body, setBody] = useState('');
     const [author, setAuthor] = useState('mario')
-     console.log(title);
-    return (
-        
+    const[isPending, setIsPending] = useState(false);
+    const  history = useHistory();
+
+     const handleSubmit = (e)=>{
+         e.preventDefault();
+         const blog = { title, body, author };
+         console.log(blog);
+         setIsPending(true);
+         fetch('http://localhost:8000/blogs', {
+             method: 'POST',
+             headers: {"Content-type": "application/json"},
+             body: JSON.stringify(blog)
+         }).then(()=>{
+             console.log('New blog added!');
+             setIsPending(false);
+            //  history.go(-1);//go back
+            history.push('/')
+         })
+        }
+    return (        
         <div className="create">
             <h2>Add a new Blog</h2>
-            <form>
+            <form onSubmit = {handleSubmit}>
                 <label>Blog title:</label>
                 <input
                 type="text"
@@ -25,12 +43,14 @@ const Create = () => {
                <label>Blog author:</label>
                <select
                value={author}
-               onChange={(e)=>(e.target.value)}
+               onChange={(e)=>setAuthor(e.target.value)}
                >
                <option value="mario">mario</option>
                <option value="yoshi">yoshi</option>
                </select>
-               <button>Add Blog</button>
+               {!isPending&&<button>Add Blog</button>}
+               {isPending&&<button disabled>Adding blog...</button>}
+
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
